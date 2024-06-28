22daaf21f68bc4e9f10991eaa93f234449f6bc98
Shreya3145 <sudhashreya07@gmail.com>
2024-06-28 23:59:10 +0530

    Java Strings Introduction

---
+++ stringintro.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class stringintro {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        System.out.println(A.length()+B.length()); //finding the lengths and adding them
+        char a =A.charAt(0);//checking lexicographically whether a is greater than b
+        char b =B.charAt(0);
+        if(a>b){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        String a1=A.substring(0,1).toUpperCase()+A.substring(1);//capitalize the 1st letter
+        String b1=B.substring(0,1).toUpperCase()+B.substring(1);
+        System.out.println(a1+ " " +b1);
+        
+    }
+}
