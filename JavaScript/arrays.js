ba81d98b0da051d64a4230ab94aee840a4c1402f
Shreya Ravichandiran <sudhashreya07@gmail.com>
2025-04-01 00:08:55 +0530

    10 days of JS

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -1,13 +1,13 @@
 function getSecondLargest(nums) {
-    let max = -Infinity;
-    let secondMax = -Infinity;
-    for(let i = 0; i < nums.length; i++){
-         if(nums[i] > max){
-             secondMax = max;
-             max = nums[i];
-         } else if(nums[i] > secondMax && nums[i] < max){
-             secondMax = nums[i];
-         }
-     }
-     return secondMax;
- }
+  let max = -Infinity;
+  let secondMax = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > max) {
+      secondMax = max;
+      max = nums[i];
+    } else if (nums[i] > secondMax && nums[i] < max) {
+      secondMax = nums[i];
+    }
+  }
+  return secondMax;
+}
